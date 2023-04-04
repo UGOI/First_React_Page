4c80b4f8139c58af5a3920503b87b7c6e5ec6480
Stefan Dukic <96357300+UGOI@users.noreply.github.com>
2023-04-04 20:51:45 +0200

    feat: update components

--- my-simple-react-app/src/components/Installation.js
+++ my-simple-react-app/src/components/Installation.js
@@ -1,18 +1,29 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 function Installation() {
+  const [buttonText, setButtonText] = useState('Copy');
+
   const copyToClipboard = () => {
     const installCommand = 'bash -c "$(curl -fsSL https://ugoi.github.io/AICommand_Page/download/install.sh)"';
     navigator.clipboard.writeText(installCommand);
+    setButtonText('Copied!');
+
+    setTimeout(() => {
+      setButtonText('Copy');
+    }, 1500);
   };
 
   return (
     <section id="installation" className="container">
       <h2 className="mt-5">Installation</h2>
       <p>Install AICommand with just one command and start using natural language to interact with your terminal.</p>
-      <div className="command-container d-inline-flex">
-        <pre className="mb-0"><code id="install-command">bash -c "$(curl -fsSL https://ugoi.github.io/AICommand_Page/download/install.sh)"</code></pre>
-        <button id="copy-button" className="btn btn-primary ml-2" onClick={copyToClipboard}>Copy</button>
+      <div className="command-container d-flex">
+        <pre className="mb-0 command-code" style={{ backgroundColor: 'black', color: 'white', padding: '5px', borderRadius: '5px', marginRight: '10px', lineHeight: '2' }}>
+          <code id="install-command">{`bash -c "$(curl -fsSL https://ugoi.github.io/AICommand_Page/download/install.sh)"`}</code>
+        </pre>
+        <button id="copy-button" className="btn btn-primary" onClick={copyToClipboard} style={{transition: '0.3s'}}>
+          {buttonText}
+        </button>
       </div>
     </section>
   );
--- my-simple-react-app/src/components/Uninstallation.js
+++ my-simple-react-app/src/components/Uninstallation.js
@@ -1,18 +1,29 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 function Uninstallation() {
-  const copyUninstallCommandToClipboard = () => {
+  const [buttonText, setButtonText] = useState('Copy');
+
+  const copyToClipboard = () => {
     const uninstallCommand = 'bash -c "$(curl -fsSL https://ugoi.github.io/AICommand_Page/download/uninstall.sh)"';
     navigator.clipboard.writeText(uninstallCommand);
+    setButtonText('Copied!');
+
+    setTimeout(() => {
+      setButtonText('Copy');
+    }, 1500);
   };
 
   return (
     <section id="uninstallation" className="container">
       <h2 className="mt-5">Uninstallation</h2>
       <p>Uninstall AICommand with just one command and remove it from your system.</p>
-      <div className="command-container d-inline-flex">
-        <pre className="mb-0"><code id="uninstall-command">bash -c "$(curl -fsSL https://ugoi.github.io/AICommand_Page/download/uninstall.sh)"</code></pre>
-        <button id="uninstall-copy-button" className="btn btn-danger ml-2" onClick={copyUninstallCommandToClipboard}>Copy</button>
+      <div className="command-container d-flex">
+        <pre className="mb-0 command-code" style={{ backgroundColor: 'black', color: 'white', padding: '5px', borderRadius: '5px', marginRight: '10px', lineHeight: '2' }}>
+          <code id="uninstall-command">{`bash -c "$(curl -fsSL https://ugoi.github.io/AICommand_Page/download/uninstall.sh)"`}</code>
+        </pre>
+        <button id="uninstall-copy-button" className="btn btn-danger" onClick={copyToClipboard} style={{transition: '0.3s'}}>
+          {buttonText}
+        </button>
       </div>
     </section>
   );
